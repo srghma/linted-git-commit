66cb7c37de0eddd38fe598392cb344142c0ad854
BjornMelgaard <melgaardbjorn@gmail.com>
2017-10-23 02:00:24 +0300

    refactor

--- src/functions/hint/type-hint/stringify-info.ts
+++ src/functions/hint/type-hint/stringify-info.ts
@@ -1,13 +1,24 @@
 import * as R from 'ramda'
+import * as RA from 'ramda-adjunct'
 import { hint } from '~/colors'
 
+const getNames = R.keys
+const getDescrs = R.pipe(R.values, R.map(R.prop('description')))
+
+const pad = (arr: string[]) => {
+  const lengths = R.map(R.prop('length') as any)(arr)
+  const maxLength = R.apply(Math.max, lengths)
+  console.log(arr, lengths)
+  console.log(maxLength)
+}
+
 export default function stringifyInfo(info: object) {
-  const typeDescriptions = R.mapObjIndexed(v => R.prop('description', v), info)
+  const rows = R.converge(R.zipObj, [
+    R.pipe(getNames, R.map(RA.concatRight(':')), pad),
+    R.pipe(getDescrs, R.map(hint)),
+  ])(info)
 
-  const coloredTypeDescriptions = R.mapObjIndexed(v => hint(v), typeDescriptions)
-  const rows = R.toPairs(coloredTypeDescriptions)
-
-  const strings = R.map(R.join(': '))(rows)
+  const strings: string[] = R.map(R.join(' '))(rows)
   const table = R.join('\n', strings)
   return table
 }
