672d46bfd7d83b58cd5784a090a9dd3d133790eb
BjornMelgaard <melgaardbjorn@gmail.com>
2017-11-26 15:04:47 +0200

    asdf

--- src/functions/hint/index.ts
+++ src/functions/hint/index.ts
@@ -4,6 +4,7 @@ import typeHint from './type-hint'
 
 export async function hint() {
   const lintConfig = await core.load({}, { cwd: process.cwd() })
+  const a = "a"
 
   console.log(typeHint(lintConfig))
 }
